15dff5e180beda20040b9d445b832dc877c9ca49
root <root@unais-Inspiron-1545.(none)>
2012-06-24 23:43:03 +0530

    LED2_blinks_when_dark-----Else_off

---
+++ same_led__as_sensor_as_well_as_light_up.c
@@ -0,0 +1,42 @@
+#include <msp430.h>
+
+void adc_init()
+{
+        ADC10CTL0 = ADC10ON | ADC10SHT_2 | SREF_0;
+        ADC10CTL1 = INCH_0 | SHS_0 | ADC10DIV_0 | ADC10SSEL_0 | CONSEQ_0 ;
+        ADC10AE0 = BIT0;
+        ADC10CTL0 |= ENC ;
+}
+
+void start_conversion()
+{
+        ADC10CTL0 |= ADC10SC;
+}
+
+unsigned int converting()
+{
+        return ADC10CTL1 & ADC10BUSY;
+}
+
+ 
+
+
+void main()
+{
+        int i;
+        P1DIR = 0x40;
+        P1OUT = 0x0;
+        while(1)
+        {
+		adc_init();
+                start_conversion();
+                if(!converting())
+		{
+			P1DIR = 0x1;
+                	P1OUT = (ADC10MEM < 0x17C)?0x1:0x0;
+		}
+                for (i=0;i<10000;i++);
+
+        }
+}
+
