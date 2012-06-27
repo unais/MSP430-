aad4554619d877d6fa19a4b6d5a556b5f9c94750
unais <tpunais@gmail.com>
2012-06-27 08:54:56 +0530

    watchdog as an intervel timer

---
+++ watchdog.c
@@ -0,0 +1,19 @@
+#include<msp430.h>
+#define LED1 BIT0
+__attribute__((interrupt(WDT_VECTOR)))
+void isr()
+{
+	P1OUT ^= LED1;
+}
+void watchdog_init()
+{
+	WDTCTL = WDTPW|WDTTMSEL;
+	IE1 = WDTIE;
+}
+main()
+{
+	P1DIR = LED1;
+	__enable_interrupt();
+	watchdog_init();
+	while(1);
+}
