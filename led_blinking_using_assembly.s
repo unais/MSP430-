d1dc8a88d6b5430f58a0900a996b0ad3a3ad45e3
unais <tpunais@gmail.com>
2012-07-16 08:49:27 +0530

    led blinking program using assemby

---
+++ led_blinking_using_assembly.s
@@ -0,0 +1,17 @@
+org 0xf800
+start:
+mov.w #0x5a80, &0x0120   
+mov.b #0x1, &0x22
+mov.b #0x0, &0x21
+L1:
+	xor.b #0x1,&0x21
+	mov.w #0xffff,R7
+L2:
+	sub.w #1,R7
+	jnz L2
+	jmp L1
+loop:
+	jmp loop
+	org 0xfffe
+	dw start
+
